0474053192a4a308d2b9d9fa5f12ab66422d2f2d
ulshinvv <158988350+ulshinvv@users.noreply.github.com>
2024-03-18 13:29:08 +0700

    Add files via upload

---
+++ Libray/Class1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carss
+{
+    public class Car
+    {
+        public string Name { get; set; }
+
+        public int MaxSpeed { get; set; }
+
+        public int Distance { get; set; }
+
+        public int CurrentSpeed { get; set; }
+
+        public Car(string name, int maxSpeed, int distance)
+        {
+            Name = name;
+            MaxSpeed = maxSpeed;
+            Distance = distance;
+            CurrentSpeed = 0;
+        }
+
+        public void Accelerate()
+        {
+            if (CurrentSpeed < MaxSpeed)
+            {
+                CurrentSpeed += 10;
+                if (CurrentSpeed > MaxSpeed)
+                {
+                    CurrentSpeed = MaxSpeed;
+                }
+            }
+        }
+
+        public void Decelerate()
+        {
+            if (CurrentSpeed > 0)
+            {
+                CurrentSpeed -= 5;
+                if (CurrentSpeed < 0)
+                {
+                    CurrentSpeed = 0;
+                }
+            }
+        }
+
+        public void Move()
+        {
+            Accelerate();
+            Distance += CurrentSpeed;
+        }
+    }
+
+   
+    public class Sedan : Car
+    {
+        public Sedan(string name, int maxSpeed, int distance) : base(name, maxSpeed, distance)
+        {
+          
+        }
+    }
+
+    
+    public class Truck : Car
+    {
+        public Truck(string name, int maxSpeed, int distance) : base(name, maxSpeed, distance)
+        {
+           
+        }
+    }
+
+    
+    public class Motorcycle : Car
+    {
+        public Motorcycle(string name, int maxSpeed, int distance) : base(name, maxSpeed, distance)
+        {
+           
+        }
+    }
+
+    
+    public abstract class Factory
+    {
+        public abstract Car CreateCar(string name, int maxSpeed, int distance);
+    }
+
+   
+    public class SedanFactory : Factory
+    {
+        public override Car CreateCar(string name, int maxSpeed, int distance)
+        {
+            return new Sedan(name, maxSpeed, distance);
+        }
+    }
+
+    
+    public class TruckFactory : Factory
+    {
+        public override Car CreateCar(string name, int maxSpeed, int distance)
+        {
+            return new Truck(name, maxSpeed, distance);
+        }
+    }
+
+    
+    public class MotorcycleFactory : Factory
+    {
+        public override Car CreateCar(string name, int maxSpeed, int distance)
+        {
+            return new Motorcycle(name, maxSpeed, distance);
+        }
+    }
+}
---
+++ TestCar/Program.cs
@@ -0,0 +1,39 @@
+﻿using Carss;
+namespace CarRace
+{
+    public class Program
+    {
+        static void Main()
+        {
+            List<Car> cars = new List<Car>
+            {
+            new Car("Sedan", 150, 0),
+            new Car("Truck", 140, 0),
+            new Car("Motorcycle", 160, 0)
+            };
+
+            while (true)
+            {
+                Console.Clear();
+                foreach (var car in cars)
+                {
+                    car.Move();
+                    Console.WriteLine($"Участник: {car.Name}, Speed: {car.CurrentSpeed}, Distance: {car.Distance}");
+                }
+
+                if (cars.TrueForAll(c => c.Distance >= 1000))
+                {
+                    Console.WriteLine("\nГонка завершена!");
+                    cars.Sort((x, y) => y.Distance.CompareTo(x.Distance));
+                    for (int i = 0; i < cars.Count; i++)
+                    {
+                        Console.WriteLine($"Место: {i + 1}: {cars[i].Name}");
+                    }
+                    break;
+                }
+
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
